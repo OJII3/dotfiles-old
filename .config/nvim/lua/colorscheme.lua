bf71faf2d9c18b43143b9472d5670b8817a18cfb
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-06-09 16:29:20 +0900

    update

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,17 +1,16 @@
--- vim.cmd([[
--- set termguicolors
+-- vim.cmd([[ set termguicolors
 -- let ayucolor="light"
 -- colorscheme ayu
 -- ]])
 
-require('tokyonight').setup({ transparent = true })
-vim.cmd([[
-colorscheme tokyonight-moon
-]])
-
+-- require('tokyonight').setup({ transparent = true })
 -- vim.cmd([[
--- colorscheme tokyonight-day
+-- colorscheme tokyonight-moon
 -- ]])
+
+vim.cmd([[
+colorscheme tokyonight-day
+]])
 
 -- require('nightfox').setup({ opstions = { transparent = true, } })
 -- vim.cmd([[
