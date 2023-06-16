ea2513028aafae3494ba0bf0f371300eeef4d1a0
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-06-16 11:15:45 +0900

    update

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,52 +1,58 @@
 local keymap = vim.keymap
 
 -- builtin
-keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
-keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
-keymap.set('n', '<Space>.', ':e $MYVIMRC<CR>')
-keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true, silent = true })
-keymap.set('t', '<C-[>', '<C-\\><C-n>', { noremap = true, silent = true })
-keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { noremap = true, silent = true })
-keymap.set('n', '<C-s>gg', '<cmd>wincmd v<CR><cmd>term<CR>git log --graph --all<CR>',
-  { noremap = true, silent = true })
-keymap.set('n', '<C-s>gd', '<cmd>wincmd v<CR><cmd>term<CR>git diff<CR>',
-  { noremap = true, silent = true })
+keymap.set("n", "j", "gj", { noremap = true, silent = true })
+keymap.set("n", "k", "gk", { noremap = true, silent = true })
+keymap.set("n", "<Space>.", ":e $MYVIMRC<CR>")
+keymap.set("t", "<ESC>", "<C-\\><C-n>", { noremap = true, silent = true })
+keymap.set("t", "<C-[>", "<C-\\><C-n>", { noremap = true, silent = true })
+keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>", { noremap = true, silent = true })
 
 -- plugins
 -- fern
-keymap.set('n', '<Space>e', "<cmd>Fern . -drawer -toggle<CR>", { noremap = true, silent = true })
+keymap.set("n", "<Space>e", "<cmd>Fern . -drawer -toggle<CR>", { noremap = true, silent = true })
 -- telescope
-keymap.set('n', '<Space>g', '<cmd>Telescope git_files<CR>', { noremap = true, silent = true })
-keymap.set('n', '<Space>f', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
-keymap.set('n', '<Space>o', '<cmd>Telescope oldfiles<cr>', { noremap = true, silent = true })
-keymap.set('n', '<Space>r', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+keymap.set("n", "<Space>g", "<cmd>Telescope git_files<CR>", { noremap = true, silent = true })
+keymap.set("n", "<Space>f", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+keymap.set("n", "<Space>o", "<cmd>Telescope oldfiles<cr>", { noremap = true, silent = true })
+keymap.set("n", "<Space>r", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
 -- lightspeed
-keymap.set('n', 's', '<Plug>Lightspeed_s', { noremap = true, silent = true })
+keymap.set("n", "s", "<Plug>Lightspeed_s", { noremap = true, silent = true })
 -- coc
-keymap.set('i', '<C-Space>', 'coc#refresh()', { expr = true, silent = true })       -- trigger completion
-keymap.set('n', '<Leader>rn', '<Plug>(coc-rename)', { silent = true })              -- rename
-keymap.set('n', '<Leader>fm', '<Plug>(coc-format)', { silent = true })              -- format
-keymap.set('n', '<Leader>ca', '<Plug>(coc-codeaction-selected)', { silent = true }) -- code action
-keymap.set('n', '<Leader>rf', '<Plug>(coc-references)', { silent = true })          -- references
+keymap.set("i", "<C-Space>", "coc#refresh()", { expr = true, silent = true }) -- trigger completion
+keymap.set("n", "<Leader>rn", "<Plug>(coc-rename)", { silent = true }) -- rename
+keymap.set("n", "<Leader>fm", "<Plug>(coc-format)", { silent = true }) -- format
+keymap.set("n", "<Leader>ca", "<Plug>(coc-codeaction-selected)", { silent = true }) -- code action
+keymap.set("n", "<Leader>rf", "<Plug>(coc-references)", { silent = true }) -- references
+keymap.set("n", "<Leader>gd", "<Plug>(coc-definition)", { silent = true }) -- go to definition
 -- skkeleton
-keymap.set('i', '<C-j>', '<Plug>(skkeleton-enable)', { noremap = false, silent = true })
-keymap.set('i', '<C-k>', '<Plug>(skkeleton-disable)', { noremap = false, silent = true })
-keymap.set('c', '<C-j>', '<Plug>(skkeleton-enable)', { noremap = false, silent = true })
-keymap.set('c', '<C-k>', '<Plug>(skkeleton-disable)', { noremap = false, silent = true })
+keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)", { noremap = false, silent = true })
+keymap.set("i", "<C-k>", "<Plug>(skkeleton-disable)", { noremap = false, silent = true })
+keymap.set("c", "<C-j>", "<Plug>(skkeleton-enable)", { noremap = false, silent = true })
+keymap.set("c", "<C-k>", "<Plug>(skkeleton-disable)", { noremap = false, silent = true })
 -- markdown-preview
-keymap.set('n', '<Space>m', '<Plug>MarkdownPreviewToggle', { noremap = false, silent = true })
+keymap.set("n", "<Space>m", "<Plug>MarkdownPreviewToggle", { noremap = false, silent = true })
 -- toggleterm
-keymap.set('n', '<C-s>v', '<cmd>ToggleTerm direction=vertical size=100<CR>', { noremap = true, silent = true })
-keymap.set('n', '<C-s>h', '<cmd>ToggleTerm direction=horizontal size=12<CR>', { noremap = true, silent = true })
-keymap.set('n', '<C-s>f', '<cmd>ToggleTerm direction=float<CR>', { noremap = true, silent = true })
-keymap.set('t', '<C-s>', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true })
-keymap.set('n', '<C-s>l', '<cmd>lua Lazygit_toggle()<CR>', { noremap = true, silent = true })
+keymap.set("n", "<C-s>v", "<cmd>ToggleTerm direction=vertical size=100<CR>", { noremap = true, silent = true })
+keymap.set("n", "<C-s>h", "<cmd>ToggleTerm direction=horizontal size=12<CR>", { noremap = true, silent = true })
+keymap.set("n", "<C-s>f", "<cmd>ToggleTerm direction=float<CR>", { noremap = true, silent = true })
+keymap.set("t", "<C-s>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+keymap.set("n", "<C-s>l", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+-- barbar
+keymap.set("n", "<C-b>j", "<cmd>bnext<CR>", { noremap = true, silent = true })
+keymap.set("n", "<C-b>k", "<cmd>bprev<CR>", { noremap = true, silent = true })
 -- luasnip
-keymap.set('i', '<Tab>', '<Plug>luasnip-expand-or-jump',
-  { noremap = false, silent = true, expr = true, callback = function() return require('luasnip').jumpable(1) end })
-keymap.set('i', '<S-Tab>', '<cmd>lua require"luasnip".jump(-1)', { noremap = false, silent = true })
-keymap.set('s', '<Tab>', '<cmd>lua require"luasnip".jump(1)<CR>', { noremap = false, silent = true })
-keymap.set('s', '<S-Tab>', '<cmd>lua require"luasnip".jump(-1)<CR>', { noremap = false, silent = true })
+-- keymap.set("i", "<Tab>", "<Plug>luasnip-expand-or-jump", {
+-- 	noremap = false,
+-- 	silent = true,
+-- 	expr = true,
+-- 	callback = function()
+-- 		return require("luasnip").jumpable(1)
+-- 	end,
+-- })
+-- keymap.set("i", "<S-Tab>", '<cmd>lua require"luasnip".jump(-1)', { noremap = false, silent = true })
+-- keymap.set("s", "<Tab>", '<cmd>lua require"luasnip".jump(1)<CR>', { noremap = false, silent = true })
+-- keymap.set("s", "<S-Tab>", '<cmd>lua require"luasnip".jump(-1)<CR>', { noremap = false, silent = true })
 -- keymap.set('i', '<C-E>', 'luasnip#choice_active() ? <Plug>luasnip-next-choice : "<C-E>"',
 --   { expr = true, noremap = false, silent = true })
 -- keymap.set('s', '<C-E>', 'luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<C-E>"',
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,14 @@
 -- auto bootstrap lazy.nvim ======================================================
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    o 'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		o("clone"),
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,131 +16,154 @@ vim.opt.rtp:prepend(lazypath)
 
 -- vim.g.mapleader = ' ' -- make sure to set `mapleader` before lazy so your mappings are correct
 
-require('lazy').setup({
-  { 'folke/which-key.nvim' },
-  {
-    'folke/neoconf.nvim', cmd = 'Neoconf',
-  },
-  { 'nvim-lua/plenary.nvim' },
-  {
-    'nvim-tree/nvim-web-devicons', lazy = true
-  },
-  {
-    'lambdalisue/fern.vim',
-    dependencies = {
-      'lambdalisue/nerdfont.vim',
-      'lambdalisue/fern-renderer-nerdfont.vim',
-      'lambdalisue/fern-git-status.vim',
-      'lambdalisue/glyph-palette.vim',
-    }
-  },
-  {
-    'lambdalisue/nerdfont.vim', lazy = true
-  },
-  {
-    'lambdalisue/fern-git-status.vim', lazy = true
-  },
-  {
-    'lambdalisue/fern-renderer-nerdfont.vim', lazy = true
-  },
-  {
-    'lambdalisue/glyph-palette.vim', lazy = true
-  },
-  { 'nvim-lualine/lualine.nvim' },
-  -- colorscheme ----------------
-  {
-    'folke/tokyonight.nvim', lazy = true
-  },
-  {
-    'EdenEast/nightfox.nvim', lazy = true
-  },
-  {
-    'ayu-theme/ayu-vim', lazy = true
-  },
-  ------------------------------
-  {
-    'neoclide/coc.nvim', branch = 'release',
-  },
-  -- lsp
-  -- { 'neovim/nvim-lspconfig' },
-  -- { 'jose-elias-alvarez/null-ls.nvim' },
-  -- { 'williamboman/mason-lspconfig.nvim' },
-  -- { 'onsails/lspkind-nvim' },
-  -- { 'hrsh7th/nvim-cmp' },
-  -- { 'hrsh7th/cmp-nvim-lsp' },
-  -- { 'hrsh7th/cmp-vsnip' },
-  -- { 'hrsh7th/cmp-buffer' },
-  -- { 'hrsh7th/cmp-path' },
-  -- { 'hrsh7th/cmp-cmdline' },
-  -- { 'hrsh7th/cmp-nvim-lsp-signature-help' },
-  -- { 'onsails/lspkind.nvim' },
-  -- { 'williamboman/mason.nvim', build = ":MasonUpdate" },
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = { 'rafamadriz/friendly-snippets' },
-  },
-  { 'rafamadriz/friendly-snippets' },
-  {
-    'windwp/nvim-autopairs',
-    init = function() require('nvim-autopairs').setup() end
-  },
-  {
-    'kylechui/nvim-surround',
-    init = function() require('nvim-surround').setup() end
-  },
-  { 'lewis6991/gitsigns.nvim' },
-  { 'nvim-telescope/telescope.nvim' },
-  { 'windwp/nvim-ts-autotag' },
-  { 'ggandor/lightspeed.nvim' },
-  {
-    'glepnir/lspsaga.nvim', branch = 'main'
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    init = function() require('nvim-treesitter.install').update({ with_sync = true }) end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  },
-  { 'github/Copilot.vim' },
-  { 'vim-denops/denops.vim' },
-  {
-    'vim-skk/skkeleton',
-    dependencies = { 'vim-denops/denops.vim' }
-  },
-  {
-    'lambdalisue/kensaku.vim',
-    dependencies = { 'vim-denops/denops.vim' }
-  },
-  {
-    'ryicoh/deepl.vim',
-    lazy = true
-  },
-  -- { 'haya14busa/vim-edgemotion' },
-  { 'tpope/vim-commentary' },
-  { 'johngrib/vim-game-code-break' },
-  {
-    'iamcco/markdown-preview.nvim',
-    config = function() vim.fn["mkdp#util#install"]() end
-  },
-  {
-    'akinsho/toggleterm.nvim', config = true
-  },
-  {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-  { 'TimUntersberger/neogit' },
-  { 'vim-latex/vim-latex' },
-  { 'lervag/vimtex' },
-  { 'dstein64/vim-startuptime' },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'lewis6991/gitsigns.nvim' },
-    init = function() vim.g.barbar_auto_setup = false end,
-  },
-  {
-    'andweeb/presence.nvim',
-  }
+require("lazy").setup({
+	{ "folke/which-key.nvim" },
+	{
+		"folke/neoconf.nvim",
+		cmd = "Neoconf",
+	},
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+	},
+	{
+		"lambdalisue/fern.vim",
+		dependencies = {
+			"lambdalisue/nerdfont.vim",
+			"lambdalisue/fern-renderer-nerdfont.vim",
+			"lambdalisue/fern-git-status.vim",
+			"lambdalisue/glyph-palette.vim",
+		},
+	},
+	{
+		"lambdalisue/nerdfont.vim",
+		lazy = true,
+	},
+	{
+		"lambdalisue/fern-git-status.vim",
+		lazy = true,
+	},
+	{
+		"lambdalisue/fern-renderer-nerdfont.vim",
+		lazy = true,
+	},
+	{
+		"lambdalisue/glyph-palette.vim",
+		lazy = true,
+	},
+	{ "nvim-lualine/lualine.nvim" },
+	-- colorscheme ----------------
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = true,
+	},
+	{
+		"ayu-theme/ayu-vim",
+		lazy = true,
+	},
+	------------------------------
+	{
+		"neoclide/coc.nvim",
+		branch = "release",
+	},
+	{
+		"heavenshell/vim-jsdoc",
+	},
+	-- lsp
+	-- { 'neovim/nvim-lspconfig' },
+	-- { 'jose-elias-alvarez/null-ls.nvim' },
+	-- { 'williamboman/mason-lspconfig.nvim' },
+	-- {
+	-- 	"glepnir/lspsaga.nvim",
+	-- 	branch = "main",
+	-- },
+	-- { 'onsails/lspkind-nvim' },
+	-- { 'hrsh7th/nvim-cmp' },
+	-- { 'hrsh7th/cmp-nvim-lsp' },
+	-- { 'hrsh7th/cmp-vsnip' },
+	-- { 'hrsh7th/cmp-buffer' },
+	-- { 'hrsh7th/cmp-path' },
+	-- { 'hrsh7th/cmp-cmdline' },
+	-- { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+	-- { 'onsails/lspkind.nvim' },
+	-- { 'williamboman/mason.nvim', build = ":MasonUpdate" },
+	{
+		"L3MON4D3/LuaSnip",
+		lazy = true,
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{ "rafamadriz/friendly-snippets", lazy = true },
+	{
+		"windwp/nvim-autopairs",
+		init = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		init = function()
+			require("nvim-surround").setup()
+		end,
+	},
+	-- { "lewis6991/gitsigns.nvim", lazy = true },
+	{ "nvim-telescope/telescope.nvim" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "ggandor/lightspeed.nvim" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		init = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
+	{ "github/Copilot.vim" },
+	{ "vim-denops/denops.vim" },
+	{
+		"vim-skk/skkeleton",
+		dependencies = { "vim-denops/denops.vim" },
+	},
+	{
+		"lambdalisue/kensaku.vim",
+		dependencies = { "vim-denops/denops.vim" },
+	},
+	-- { "ryicoh/deepl.vim", },
+	-- { 'haya14busa/vim-edgemotion' },
+	{ "tpope/vim-commentary" },
+	{ "johngrib/vim-game-code-break" },
+	{
+		"iamcco/markdown-preview.nvim",
+		config = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+	},
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "TimUntersberger/neogit" },
+	{
+		"vim-latex/vim-latex",
+		dependencies = { "lervag/vimtex" },
+	},
+	{ "lervag/vimtex", lazy = true },
+	{ "dstein64/vim-startuptime" },
+	-- {
+	-- 	"romgrk/barbar.nvim",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons", "lewis6991/gitsigns.nvim" },
+	-- 	init = function()
+	-- 		vim.g.barbar_auto_setup = false
+	-- 	end,
+	-- },
 })
