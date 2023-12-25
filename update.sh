414304e9ed261889a06240160e156a6694ee878a
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-12-25 12:14:40 +0900

    change updage script

--- update.sh
+++ update.sh
@@ -1,18 +1,3 @@
 # !/bin/bash
 
-git add .
-git status
-
-# ask if user wants to commit
-read -p "Continue? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # commit
-    read -p "Commit message: " commitMessage
-    git commit -m "$commitMessage"
-    git push
-    echo "Done!"
-else
-  echo "Aborted."
-fi
+git pull
