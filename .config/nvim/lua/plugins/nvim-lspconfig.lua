87b22c68f02cd92d16f543e9ef8cd5e070a9bd69
OJII3 <84656786+OJII3@users.noreply.github.com>
2024-05-12 17:45:16 +0900

    update lsp

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -135,13 +135,12 @@ return {
 		})
 		vim.api.nvim_create_autocmd("LspAttach", {
 			callback = function(args)
-				local bufnr = args.buf
 				local client = vim.lsp.get_client_by_id(args.data.client_id)
 				if not client then
 					return
 				end
 				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(bufnr, true)
+					vim.lsp.inlay_hint.enable(true)
 				end
 			end,
 		})
