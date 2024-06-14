cb3bf9fde48c6f8dd052c8f2fbcaf585f1cd7c5c
OJII3 <84656786+OJII3@users.noreply.github.com>
2024-06-14 10:29:10 +0900

    fix?

--- .config/hypr/scripts/path.sh
+++
@@ -1,1 +0,0 @@
-export SDL_VIDEODRIVER=x11
--- .config/hypr/scripts/toggle_touchpad.sh
+++ .config/hypr/scripts/toggle_touchpad.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-export STATUS_FILE="$XDG_RUNTIME_DIR/keyboard.status"
+STATUS_FILE="$XDG_RUNTIME_DIR/keyboard.status"
 
 enable_keyboard() {
     printf "true" >"$STATUS_FILE"
     hyprctl keyword '$LAPTOP_KB_ENABLED' "true" -r &&
-    notify-send -u normal "Touchpad Enabled"
+      notify-send -u normal "Touchpad Enabled"
 }
 
 disable_keyboard() {
     printf "false" >"$STATUS_FILE"
     hyprctl keyword '$LAPTOP_KB_ENABLED' "false" -r &&
-    notify-send -u normal "Touchpad Disabled"
+      notify-send -u normal "Touchpad Disabled"
 }
 
 if ! [ -f "$STATUS_FILE" ]; then
@@ -23,3 +23,5 @@ else
     enable_keyboard
   fi
 fi
+
+exit 0
